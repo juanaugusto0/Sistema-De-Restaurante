1b4d41a248298785db42b5321a471ee7e20b1c79
Juan Augusto Gomes Vieira <jg14253@gmail.com>
2025-02-24 17:49:05 -0300

    first commit

---
+++ sistemaderestaurante/src/main/java/com/study/sistemaderestaurante/SistemaderestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.study.sistemaderestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaderestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaderestauranteApplication.class, args);
+	}
+
+}
---
+++ sistemaderestaurante/src/test/java/com/study/sistemaderestaurante/SistemaderestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sistemaderestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaderestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
